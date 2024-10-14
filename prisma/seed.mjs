f5422fc22674c18fa37adb01ec2b82e1e5a15e05
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-14 23:17:20 +0500

    swagger

---
+++ prisma/seed.mjs
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient();
+
+const tempUser = {
+	email: 'rasul@rasul.com',
+	password: 'rasul'
+};
+
+const posts = [
+	{
+		title: 'new Post',
+		content: 'new Content'
+	}
+];
+async function bootstrap() {
+	await prisma.$connect();
+	const createdUser = await prisma.user.create({ data: tempUser });
+	await prisma.post.createMany({
+		data: posts.map(post => ({...post, authorId: createdUser.id }))
+	})
+	await prisma.$disconnect();
+}
+bootstrap();
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,36 @@ const prisma = new PrismaClient();
 
 export class App {
     async init () {
-        await prisma.$connect();
-        await prisma.user.count()
+        const connectDB = await prisma.$connect();
+        await prisma.post.findFirst({
+            where: {
+                title: ''
+            }
+        });
+        await prisma.post.findUnique({
+            where: {
+                id: 1,
+            }
+        });
+        await prisma.user.findMany({
+            where: {
+                id: {
+                    gte: 1
+                }
+            }
+        });
+        await prisma.post.findMany({
+            where: {
+                tags: {
+                    some: {
+                        tag: {
+                            name: 'my tah'
+                        }
+                    }
+                }
+            }
+        });
+        await prisma.$queryRaw`SELECT * FROM "posts"`
     }
 }
 
