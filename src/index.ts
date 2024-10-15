be361d63dfbac66718652b11a32a44759accf12c
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-15 16:10:33 +0500

    swagger

--- src/index.ts
+++ src/index.ts
@@ -7,35 +7,35 @@ const prisma = new PrismaClient();
 export class App {
     async init () {
         const connectDB = await prisma.$connect();
-        await prisma.post.findFirst({
-            where: {
-                title: ''
-            }
-        });
-        await prisma.post.findUnique({
-            where: {
-                id: 1,
-            }
-        });
-        await prisma.user.findMany({
-            where: {
-                id: {
-                    gte: 1
-                }
-            }
-        });
-        await prisma.post.findMany({
-            where: {
-                tags: {
-                    some: {
-                        tag: {
-                            name: 'my tah'
-                        }
-                    }
-                }
-            }
-        });
-        await prisma.$queryRaw`SELECT * FROM "posts"`
+        // await prisma.post.findFirst({
+        //     where: {
+        //         title: ''
+        //     }
+        // });
+        // await prisma.post.findUnique({
+        //     where: {
+        //         id: 1,
+        //     }
+        // });
+        // await prisma.user.findMany({
+        //     where: {
+        //         id: {
+        //             gte: 1
+        //         }
+        //     }
+        // });
+        // await prisma.post.findMany({
+        //     where: {
+        //         tags: {
+        //             some: {
+        //                 tag: {
+        //                     name: 'my tah'
+        //                 }
+        //             }
+        //         }
+        //     }
+        // });
+        // await prisma.$queryRaw`SELECT * FROM "posts"`
     }
 }
 const bootstrap = async () => {
