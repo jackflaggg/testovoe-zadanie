3f07d03d628b248d0b0c6c121f4aa0d4701c1c9b
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-15 16:23:37 +0500

    создание класса сервера

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import { PrismaClient, User } from '@prisma/client'
+import {Settings} from "./settings";
+import express, {Express} from 'express'
+import {Server} from "http";
+import cors from 'cors'
+
+const prisma = new PrismaClient();
+
+export class App {
+    private app: Express;
+    private server: Server | undefined;
+    public port: number;
+
+    constructor() {
+        this.app = express();
+        this.port = Number(Settings.port);
+    }
+
+    useMiddleware() {
+        this.app.use(express.json());
+        this.app.use(cors());
+    }
+
+    useRoutes(){
+        this.app.use('/', this.useRoutes)
+    }
+    public async init () {
+        this.useMiddleware();
+        this.useRoutes();
+        const db = await prisma.$connect();
+        this.server = this.app.listen(this.port, () => {
+            console.log('Сервер запущен на ' + this.port + ' порту!')
+        })
+    }
+}
