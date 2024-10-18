b639b55d8a8b74cdd189814be2efd8f552efd191
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-18 15:13:51 +0500

    создание контейнера с зависимостями

--- src/domain/promotion.suppliers.service.ts
+++ src/domain/promotion.suppliers.service.ts
@@ -1,1 +1,11 @@
-export class PromotionSuppliersService {}
+export class PromotionSuppliersService {
+    async loginAdmin(): Promise<void> {}
+    async createPromotion(): Promise<void> {}
+    // получение всех акций из репозитория
+    async deletePromotion(id: string): Promise<void> {}
+    async updatePromotion(id: string): Promise<void> {}
+    async createSupplier(): Promise<void> {}
+    async updatePasswordSupplier(): Promise<void> {}
+    async deleteSupplier(id: string): Promise<void> {}
+    async statusPromoToSupplier(id: string): Promise<void> {}
+}
