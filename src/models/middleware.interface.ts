fbb7f15d7c74ea1f8613a1d2bf9fab3d95c8d736
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-18 14:21:12 +0500

    создание контейнера с зависимостями

--- src/models/middleware.interface.ts
+++ src/models/middleware.interface.ts
@@ -2,5 +2,6 @@ import {NextFunction, Request, Response} from "express";
 
 //интерфейс определяет структуру для middleware-функций
 export interface MiddlewareInterface {
+    //Определяет метод, который будет выполняться при обработке запроса
     execute: (req: Request, res: Response, next: NextFunction) => void;
 }
