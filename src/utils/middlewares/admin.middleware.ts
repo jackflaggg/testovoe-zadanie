704b4bf5107ffc5520481a180d6cb8274c9a170c
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-26 15:26:41 +0500

    создание контейнера с зависимостями

--- src/utils/middlewares/admin.middleware.ts
+++ src/utils/middlewares/admin.middleware.ts
@@ -1,28 +1,7 @@
-import {MiddlewareInterface} from "../../models/middleware.interface";
-import {Request, Response, NextFunction} from "express";
-import {HTTP_STATUSES} from "../../models/http-statuses.models";
-import {fromUTF8ToBase64} from "../features/UTF8ToBase64";
-import {Settings} from "../../settings";
 import {IsEmail, IsString} from "class-validator";
 
 export class AdminLoginDto{
-    // execute(req: Request, res: Response, next:NextFunction) {
-    //     const {authorization: auth} = req.headers;
-    //     console.log(auth)
-    //     if (!auth || !auth?.startsWith('Basic')){
-    //         console.log(`[auth] ошибка в авторизации в заголовках`);
-    //         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
-    //         return;
-    //     }
-    //
-    //     const codedAuth = fromUTF8ToBase64(Settings.admin);
-    //     if (auth.slice(6) !== (codedAuth)){
-    //         console.log(`[codedAuthorization] ошибка в декодировании`);
-    //         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
-    //         return;
-    //     }
-    //     next()
-    // }
+
     @IsEmail({}, { message: 'Неверно указан email'})
     email: string;
 
