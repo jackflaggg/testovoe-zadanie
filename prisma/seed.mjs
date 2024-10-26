75c90f43a8c30cc79688d452e8ccf5e12916a0f2
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-26 14:59:07 +0500

    создание контейнера с зависимостями

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -34,12 +34,12 @@ async function bootstrap() {
 	try {
 		await prisma.$connect();
 		const createdUser = await prisma.userModel.create({ data: adminUser });
-		console.log(createdUser);
+
 		const promotionsWithSupplierId = promotions.map(promo => ({
 			...promo,
 			supplierId: createdUser.id // Используйте созданный ID пользователя
 		}));
-		console.log(promotionsWithSupplierId)
+
 		await prisma.promotionModel.createMany({
 			data: promotionsWithSupplierId
 		})
