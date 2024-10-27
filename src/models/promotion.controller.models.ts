f818c879c068ebb5f29e764058f0032f5a9f917f
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-27 19:20:27 +0500

    создание контейнера с зависимостями

--- src/models/promotion.controller.models.ts
+++ src/models/promotion.controller.models.ts
@@ -4,4 +4,9 @@ export interface PromotionControllerModels {
     login: (req: Request, res: Response, next: NextFunction) => Promise<void>;
     promotions: (req: Request, res: Response, next: NextFunction) => Promise<void>;
     registerSuppliers: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+    updateSuppliers: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+    deleteSuppliers: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+    createPromotion: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+    deletePromotion: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+    updatePromotion: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
